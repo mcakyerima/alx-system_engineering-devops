412b3889054df9a68fc21f3d2dbae1a827775974
mcakyerima <mcakyerima@gmail.com>
2024-01-03 01:04:29 +0100

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,38 @@
 #!/usr/bin/python3
 """
-Request from API; Return TODO list progress given employee ID and export to CSV
+Request from API; Return TODO list progress given employee ID
+Export this data to CSV
 """
 import csv
 import requests
 from sys import argv
 
 
-def display():
+def to_csv():
     """return API data"""
-    user_id = int(argv[1])
     users = requests.get("http://jsonplaceholder.typicode.com/users")
     for u in users.json():
-        if u.get('id') == user_id:
-            employee_name = u.get('name')
+        if u.get('id') == int(argv[1]):
+            USERNAME = (u.get('username'))
             break
+    TASK_STATUS_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
 
-    tasks = requests.get("http://jsonplaceholder.typicode.com/todos")
-    employee_tasks = []
-
-    for task in tasks.json():
-        if task.get('userId') == user_id:
-            task_completed_status = str(task.get('completed'))
-            task_title = task.get('title')
-            employee_tasks.append([str(user_id), employee_name, task_completed_status, task_title])
-
-    if employee_tasks:
-        file_name = f"{user_id}.csv"
-        with open(file_name, mode='w', newline='') as file:
-            csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-            csv_writer.writerows(employee_tasks)
-        print(f"Data exported to {file_name}")
-    else:
-        print(f"No tasks found for user ID {user_id}")
+    """export to csv"""
+    filename = "{}.csv".format(argv[1])
+    with open(filename, "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in TASK_STATUS_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                             "TASK_COMPLETED_STATUS": task[0],
+                             "TASK_TITLE": task[1]})
 
 
 if __name__ == "__main__":
-    display()
+    to_csv()
