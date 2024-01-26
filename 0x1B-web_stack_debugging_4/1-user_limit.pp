98ed363005bfa5d7ae79630a072e9cf929e53fa1
mcakyerima <mcakyerima@gmail.com>
2024-01-26 02:39:22 +0100

    fixed_code

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,22 +1,12 @@
 # Enable the user holberton to login and open files without error.
-
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
-  notify  => Exec['reload_pam'],
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft file limit for Holberton user.
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
-  notify  => Exec['reload_pam'],
-}
-
-# Reload PAM to apply the changes immediately.
-exec { 'reload_pam':
-  command => 'pkill -HUP -x -u holberton sshd',
-  path    => '/usr/local/bin/:/bin/',
-  refreshonly => true,
+  path    => '/usr/local/bin/:/bin/'
 }
